2a54a8cef6001877d6bc99cee3f57a9b25b19766
Victor Lundberg <victorlundbergiv@gmail.com>
2026-09-20 18:32:22 -0400

    new sw.js file with new repo name

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'load-calc-drill-v2';
+const CACHE_NAME = 'articles-in-a-flash-v1';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {

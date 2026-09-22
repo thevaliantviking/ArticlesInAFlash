c9de82e4533995b15b19b7b55f7c01d67992629d
Victor Lundberg <victorlundbergiv@gmail.com>
2026-09-22 12:14:34 -0400

    updated to new version of app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'articles-in-a-flash-v1';
+const CACHE_NAME = 'articles-in-a-flash-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
